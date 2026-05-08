b3ecefcabec691230f81420c2f5440751717df22
phatchara1995 <p7hgdvpnjy@privaterelay.appleid.com>
2026-05-09 00:31:58 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+self.addEventListener('install', event => {
+
+    event.waitUntil(
+
+        caches.open('audio-extractor-v1').then(cache => {
+
+            return cache.addAll([
+                './',
+                './index.html'
+            ]);
+
+        })
+
+    );
+
+});
+
+self.addEventListener('fetch', event => {
+
+    event.respondWith(
+
+        caches.match(event.request).then(response => {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
